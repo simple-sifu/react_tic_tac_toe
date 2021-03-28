2a8108cfe440b52008c09e4c05388f4b6a8876d8
Tommy Han <tommy_han1@intuit.com>
2021-03-28 02:06:17 -0400

    setup history of moves

--- src/Game.jsx
+++ src/Game.jsx
@@ -15,7 +15,7 @@ class Game extends React.Component {
 }
 
 handleClick(i){
-  const history = this.state.history
+  const history = this.state.history.slice(0, this.state.stepNumber + 1);
   const current = history[history.length - 1];
   const squares = current.squares.slice();
   if (calculateWinner(squares) || squares[i]) {
@@ -26,6 +26,7 @@ handleClick(i){
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
   });
 }
@@ -39,7 +40,7 @@ jumpTo(step) {
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
